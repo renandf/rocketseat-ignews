a123e0a7650645c9995ba865a1353e04c397cde8
Renan Castro <renandf@gmail.com>
2021-06-28 21:50:22 +1000

    feat: Update from server side rendering to static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -40,7 +40,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   // if more product info is needed, use expand
   // const price = await stripe.prices.retrieve('price_1J7IRyLyrMNh40viqdnfmvo8', {
   //   expand: ['product']
@@ -58,6 +58,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
